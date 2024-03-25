0f9f26521be91c3c8d5b7bf2c94af6ef8b86ce53
Alena <alenzija@gmail.com>
2024-03-25 11:00:51 +0100

    init: init docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import { Favorites } from './favorites/entity/favorites.entity';
       port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      database: process.env.POSTGRES_DB,
       entities: [User, Artist, Album, Track, Favorites],
       synchronize: process.env.NODE_ENV === 'development',
     }),
